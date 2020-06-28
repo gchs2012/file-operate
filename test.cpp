4bbe9267200aa5d54833391926d5fa76ebf6afa9
zhangcan <554162259@qq.com>
2020-06-28 13:30:58 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -31,5 +31,13 @@ int main(int argc, char const *argv[])
 
 
 
-int foo(int x);
-auto foo(int x) -> int;
+int price_count_reader;    // 无缩写
+int num_errors;            // "num" 是一个常见的写法
+int num_dns_connections;   // 人人都知道 "DNS" 是什么
+
+int n;                     // 毫无意义.
+int nerr;                  // 含糊不清的缩写.
+int n_comp_conns;          // 含糊不清的缩写.
+int wgc_connections;       // 只有贵团队知道是什么意思.
+int pc_reader;             // "pc" 有太多可能的解释了.
+int cstmr_id;              // 删减了若干字母.
