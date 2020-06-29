f5a0d892ded2e82102e94ead29badf6aec32fa28
zhangcan <554162259@qq.com>
2020-06-29 13:22:09 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -30,19 +30,57 @@ int main(int argc, char const *argv[])
 }
 
 
-bool retval = DoSomething(argument1, argument2, argument3);
 
-bool retval = DoSomething(averyveryveryverylongargument1,
-                          argument2, argument3);
-
-if (...) {
+if (condition) {  // 圆括号里没有空格.
+  ...  // 2 空格缩进.
+} else if (...) {  // else 与 if 的右括号同一行.
   ...
+} else {
   ...
-  if (...) {
-    DoSomething(
-        argument1, argument2,  // 4 空格缩进
-        argument3, argument4);
-  }
+}
 
+if ( condition ) {  // 圆括号与空格紧邻 - 不常见
+  ...  // 2 空格缩进.
+} else {  // else 与 if 的右括号同一行.
+  ...
+}
 
+if(condition)     // 差 - IF 后面没空格.
+if (condition){   // 差 - { 前面没空格.
+if(condition){    // 变本加厉地差.
+if (condition) {  // 好 - IF 和 { 都与空格紧邻.
+    
+if (x == kFoo) return new Foo();
+if (x == kBar) return new Bar();
+    
+// 不允许 - 当有 ELSE 分支时 IF 块却写在同一行
+if (x) DoThis();
+else DoThat();
+    
+if (condition)
+  DoSomething();  // 2 空格缩进.
+
+if (condition) {
+  DoSomething();  // 2 空格缩进.
+}
+    
+// 不可以这样子 - IF 有大括号 ELSE 却没有.
+if (condition) {
+  foo;
+} else
+  bar;
+
+// 不可以这样子 - ELSE 有大括号 IF 却没有.
+if (condition)
+  foo;
+else {
+  bar;
+}
+    
+// 只要其中一个分支用了大括号, 两个分支都要用上大括号.
+if (condition) {
+  foo;
+} else {
+  bar;
+}    
 
